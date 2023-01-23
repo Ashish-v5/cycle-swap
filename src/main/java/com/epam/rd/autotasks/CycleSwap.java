707118ad88f57f88eca559cc1a39230b3eaccae2
Ashish7429 <ashish.11916604@lpu.in>
2023-01-23 14:55:00 +0530

    done

--- src/main/java/com/epam/rd/autotasks/CycleSwap.java
+++ src/main/java/com/epam/rd/autotasks/CycleSwap.java
@@ -2,10 +2,15 @@ package com.epam.rd.autotasks;
 
 class CycleSwap {
     static void cycleSwap(int[] array) {
-
+        int temp = array[0];
+        for(int i=1;i<array.length;i++)
+            array[i-1]=array[i];
+        array[array.length-1]=temp;
     }
 
     static void cycleSwap(int[] array, int shift) {
-
+        for(int i=0;i<shift;i++){
+            cycleSwap(array);
+        }
     }
 }
